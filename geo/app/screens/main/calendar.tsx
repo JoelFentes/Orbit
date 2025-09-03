116fcf38ac11971c4b31396379745c91d65db735
joserlan08 <163345282+joserlan08@users.noreply.github.com>
2025-09-02 21:36:38 -0300

    feat: Update Calendar

--- geo/app/screens/main/calendar.tsx
+++ geo/app/screens/main/calendar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 LocaleConfig.locales['pt-br'] = {
   monthNames: [
@@ -22,35 +23,24 @@ export default function MyCalendar() {
   const [selectedDate, setSelectedDate] = useState('');
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {selectedDate ? `Dia selecionado: ${selectedDate}` : 'Selecione um dia'}
-      </Text>
+    <SafeAreaProvider>
+        <View className='flex-1 bg-white'>
+        <Text className="font-quicksand-semibold text-3xl text-azul-escuro mt-20 ml-6 mb-4">
+            Calendário
+        </Text>
 
-      <Calendar
-        onDayPress={(day) => setSelectedDate(day.dateString)}
-        minDate={'2025-01-01'}
-        maxDate={'2025-12-31'}
-        markingType={'multi-dot'}
-        markedDates={{
-          '2025-09-05': { selected: true, marked: true, selectedColor: '#78c0e0ff' },
-          '2025-09-07': { marked: true, dotColor: 'red' },
-        }}
-      />
-    </View>
+        <Calendar
+            onDayPress={(day) => setSelectedDate(day.dateString)}
+            minDate={'2025-01-01'}
+            maxDate={'2025-12-31'}
+            markingType={'multi-dot'}
+            markedDates={{
+            '2025-09-05': { selected: true, marked: true, selectedColor: '#78c0e0ff' },
+            '2025-09-07': { marked: true, dotColor: 'red' },
+            }}
+        />
+        </View>
+    </SafeAreaProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-});
